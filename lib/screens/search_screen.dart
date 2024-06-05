0dbe510bfe26a250be4bf3019c61560452cabdb7
Satoki Hata <satokihata@gmail.com>
2024-06-05 14:17:13 +0900

    update api実装

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_qiita_search/models/article.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -6,6 +10,23 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  Future<List<Article>> searchQiita(String keyword) async {
+    final uri = Uri.https('qiita.com', '/api/v2/items', {
+      'query': 'title:${keyword}',
+      'per_page': '10',
+    });
+    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+    final http.Response res = await http.get(uri, headers: {
+      'Authorization': 'Bearer $token',
+    });
+    if (res.statusCode == 200) {
+      final List<dynamic> body = jsonDecode(res.body);
+      return body.map((dynamic) => Article.fromJson(json)).toList();
+    } else {
+      return [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
